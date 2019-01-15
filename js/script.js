879fafa6c28d21d17c62314a172ec74cd07e283d
Nylah Godoy <ntng08@gmail.com>
2019-01-15 22:59:38 +0000

    buzzfeed4

--- js/script.js
+++ js/script.js
@@ -13,6 +13,54 @@ $(document).ready(function() {
 
 });
 
-function ques1(winter, spring, summer, fall){
-    
+var score = "0";
+
+function ques1(season){
+    if (season === "winter"){
+    score = score + "4"
+    return score;
+} else if (season === "summer")
+    score = score + "2"
+    return score; 
 }
+
+var scoreTwo = "0"
+
+function ques2(time){
+    if(time === "evening"){
+    scoreTwo = scoreTwo + "4";
+    return score;
+} else if (time === "morning")
+    scoreTwo = scoreTwo + "2"
+    return scroreTwo;
+}
+
+var scoreThree = "0"
+
+function ques3(element){
+    if (element === "earth"){
+    scoreThree = scoreThree + "4"
+    return scoreThree; 
+} else if (element === "fire")
+    scoreOne = scoreThree + "2"
+    return scoreThree; 
+}
+
+var scoreFour = "0"
+
+function ques4(color){
+    if (color === "yellow"){
+    scoreFour = scoreFour + "4"
+    return scoreFour;
+} else if (color === "black")
+    scoreFour = scoreFour + "2"
+    return scoreFour; 
+}
+
+totalScore = console.log()
+
+function 
+
+
+
+
